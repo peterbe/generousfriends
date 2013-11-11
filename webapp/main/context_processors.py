53666a7fdace7aad0c13bb9d448f43fdbe61644b
Peter Bengtsson <mail@peterbe.com>
2013-11-10 16:53:45 -0800

    new menu any things

--- webapp/main/context_processors.py
+++ webapp/main/context_processors.py
@@ -6,4 +6,9 @@ def base(request):
     context['DEBUG'] = settings.DEBUG
     context['PROJECT_TITLE'] = settings.PROJECT_TITLE
     context['PROJECT_STRAPLINE'] = settings.PROJECT_STRAPLINE
+    context['USE_USERSNAP'] = not settings.DEBUG
+    user_agent = request.META.get('HTTP_USER_AGENT', '')
+    if 'iPhone' in user_agent and 'Safari' in user_agent:
+        context['USE_USERSNAP'] = False
+
     return context
