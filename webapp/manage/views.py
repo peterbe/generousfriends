84b3ab8413dd098c6f5ff6092ca56b1366f09d36
Peter Bengtsson <mail@peterbe.com>
2013-11-23 11:49:13 -0800

    fix on total check

--- webapp/manage/views.py
+++ webapp/manage/views.py
@@ -123,7 +123,7 @@ def wishlists_data(request):
                     status = "PAYMENTS_STARTED"
                     first_payment, = payments.order_by('added')[:1]
                     days_left = 30 - (now - first_payment.added).days
-                    if total_amount > item.price:
+                    if total_amount >= item.price:
                         status = 'COMPLETE'
                         if item.fulfilled:
                             status = 'FULFILLED'
